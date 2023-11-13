2c22b32269631c0a1c386baa33a12d6cc2cec7c7
M. Arkan Haris <m.arkanharistkj@gmail.com>
2023-11-13 11:18:52 +0700

    initialized project

---
+++ src/main/java/com/mycompany/project_uas/tokobuku/Project_UASTokoBuku.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_uas.tokobuku;
+
+/**
+ *
+ * @author arkan
+ */
+public class Project_UASTokoBuku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
