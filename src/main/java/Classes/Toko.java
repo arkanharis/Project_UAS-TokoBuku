675195c36bd8d6c9e3339e29a7392fddb7d6c454
Giant77 <willyarsyad77@gmail.com>
2023-11-26 15:52:59 +0700

    adding java doc to toko.java, remove unused imports, customize method list daftar barang to better suit item class

--- src/main/java/Classes/Toko.java
+++ src/main/java/Classes/Toko.java
@@ -3,38 +3,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Classes;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 /**
  *
- * @author giant
+ * @author Willy Jonathan Arsyad
  */
 public class Toko {
     private ArrayList<Item> daftarBarang;
     private String namaToko; 
     
+    /**
+     * Constructor untuk class Toko
+     * @param namaToko nama toko alat tulis/toko buku
+     */
     public Toko(String namaToko) {
         this.namaToko = namaToko;
         this.daftarBarang = new ArrayList<Item>();
     }
 
+    /**
+     * Method digunakan untuk mengubah nama toko
+     * @param namaToko nama toko baru
+     */
     public void setNamaToko(String namaToko) {
         this.namaToko = namaToko;
     }
 
+    /**
+     * Menambahkan barang yang akan di jual ke dalam array list
+     * @param barangYangDijual
+     */
     public void tambahDaftarBarang(Item barangYangDijual) {
         daftarBarang.add(barangYangDijual);
     }
 
+    /**
+     * Mengembalikan nama dari toko
+     * @return nama toko
+     */
     public String getNamaToko() {
         return namaToko;
     }
 
+    /**
+     * Menambilkan daftar barang yang dijual pada toko
+     * beserta stok yang tersidia
+     */
     public void displayDaftarBarang() {
-        System.out.println("Daftar barang yang tersedia:");
+        System.out.println("Daftar barang dan stok yang tersedia:");
         for (Item barangYangDijual : daftarBarang) {
-            System.out.println(barangYangDijual);   // to-do customize print depending on item parent class 
+            System.out.println(barangYangDijual.getJenis() + "    \t" + barangYangDijual.getStok());
         }
     }
 
